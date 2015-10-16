252cb8c6c06ea3cde902c616223280db9ea47b45
deshraj <deshrajdry@gmail.com>
2015-10-17 03:50:10 +0530

    code_editor app modified and AJAX is working with support of csrftoken

--- code_editor/views.py
+++ code_editor/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic import View
 from code_editor.models import CodeSubmission
-from django.http import HttpResponseRedirect 
+from django.http import HttpResponseRedirect, HttpResponse
+import json
+
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -15,10 +17,20 @@ class CodeView(View):
 		return HttpResponseRedirect('/code/%s' %(code_obj.id))
 
 class CodeEditor(View):
+
 	def get(self, request, code_id=None):
 		template_name = 'code_editor.html'
 		code_obj = get_object_or_404(CodeSubmission, pk=code_id)
-		return render(request, template_name,)
-
+		return render(request, template_name, {'code_obj':code_obj})
+		
 	def post(self, request, code_id = None):
-		return HttpResponse()
+		try:
+			code_obj = get_object_or_404(CodeSubmission, pk=code_id)
+			new_code = request.POST.get('code','')
+			lang = request.POST.get('lang',None)
+			CodeSubmission.objects.filter(id = code_id).update(code = new_code, language = lang)
+			status =  {'status': True}
+		except Exception as e:
+			status =  {'status': False}
+		return HttpResponse(json.dumps(status), content_type="application/json")
+
