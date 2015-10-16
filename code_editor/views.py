e8bfdf020869eefa6691ed4a5cd623f91f0197f5
deshraj <deshrajdry@gmail.com>
2015-10-16 23:45:58 +0530

    CodeEditor views added to create dynamic generated urls on every request

--- code_editor/views.py
+++ code_editor/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic import View
 from code_editor.models import CodeSubmission
+from django.http import HttpResponseRedirect 
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -9,6 +10,15 @@ class IndexView(TemplateView):
 
 class CodeView(View):
 	def get(self, request):
-		obj = CodeSubmission.objects.create()
-		obj.save()
-		return HttpResponseRedirect('/code/%s' %())
+		code_obj = CodeSubmission.objects.create()
+		code_obj.save()
+		return HttpResponseRedirect('/code/%s' %(code_obj.id))
+
+class CodeEditor(View):
+	def get(self, request, code_id=None):
+		template_name = 'code_editor.html'
+		code_obj = get_object_or_404(CodeSubmission, pk=code_id)
+		return render(request, template_name,)
+
+	def post(self, request, code_id = None):
+		return HttpResponse()
