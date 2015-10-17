b0c06b3ba6eb144d0aae58c87d24957753da5897
deshraj <deshrajdry@gmail.com>
2015-10-18 02:08:51 +0530

    views edited and template modified for ACE editor working

--- code_editor/views.py
+++ code_editor/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic import View
-from code_editor.models import CodeSubmission
 from django.http import HttpResponseRedirect, HttpResponse
+from random import randint
+
+from code_editor.models import CodeSubmission
+from codetable.settings import HE_CLIENT_SECRET
+from languages import LANGUAGES
+
 import json
+import urllib
+import StringIO
+import gzip
 
 # Create your views here.
+COMPILE_URL = 'https://api.hackerearth.com/v3/code/compile/'
+RUN_URL = 'https://api.hackerearth.com/v3/code/run/'
 
 class IndexView(TemplateView):
 	template_name = "index.html"
@@ -21,9 +31,44 @@ class CodeEditor(View):
 	def get(self, request, code_id=None):
 		template_name = 'code_editor.html'
 		code_obj = get_object_or_404(CodeSubmission, pk=code_id)
-		return render(request, template_name, {'code_obj':code_obj})
+		return render(request, template_name, {'code_obj':code_obj, 'languages': LANGUAGES})
 		
 	def post(self, request, code_id = None):
+		try:
+			code_obj = get_object_or_404(CodeSubmission, pk=code_id)
+			source = request.POST.get('code','')
+			lang = request.POST.get('lang',None)
+			post_data = {
+				'client_secret': HE_CLIENT_SECRET,
+				'async': 0,
+				'source': source,
+				'lang': lang,
+				'time_limit': 5,
+				'memory_limit': 262144,
+				'id': randint(0,1000000),
+				# 'callback': 'http://localhost:8000/receive-hackerearth-response/'
+			}
+			post_data = urllib.urlencode(post_data)
+			response = urllib.urlopen(RUN_URL, post_data)
+			# See if the response is gzipped or not 
+			if response.info().get('Content-Encoding') == 'gzip':
+				buf = StringIO.StringIO( response.read())
+				gzip_f = gzip.GzipFile(fileobj=buf)
+				content = gzip_f.read()
+			else:
+				content = response.read()
+			# Update the code written by user in the database table
+			CodeSubmission.objects.filter(id = code_id).update(code = source, language = lang)
+			return HttpResponse(json.dumps(content), content_type="application/json")
+		except Exception as e:
+			return HttpResponse(json.dumps(e), content_type="application/json")
+		# Return the response after making the API call 
+
+class SaveCurrentCode(View):
+	def post(self, request, code_id = None):
+		"""
+		Method to save the code instantly as there is some change in the code.
+		"""
 		try:
 			code_obj = get_object_or_404(CodeSubmission, pk=code_id)
 			new_code = request.POST.get('code','')
