66e246e772d41b04dfebe79f706a245145fac4de
deshraj <deshrajdry@gmail.com>
2015-10-18 04:36:56 +0530

    Models modified

--- code_editor/models.py
+++ code_editor/models.py
@@ -11,15 +11,9 @@ class CodeSubmission(models.Model):
 	'''
 	To store the codesubmissions made by users
 	'''
-	LANGUAGES = (
-		('PY','Python'),
-		('C','C language'),
-		('C++','C++'),
-		('JAVA','Java'),
-		)
 	DEFAULT_CODE = "print 'Hello World'"
 	id = models.CharField(max_length = 6, primary_key=True, default=id_generator, editable=False)
 	code = models.TextField(default = DEFAULT_CODE)
-	language = models.CharField(max_length = 4, choices = LANGUAGES, default = 'PY')
+	language = models.CharField(max_length = 40, default = 'PYTHON')
 	created_at = models.DateTimeField(auto_now_add = True)
 
